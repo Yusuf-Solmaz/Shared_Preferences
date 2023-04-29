0443ed84d68cf4d6a51990b0dc53e6bf9c71b374
Yusuf-Solmaz <yusufmucahitsolmaz@gmail.com>
2023-04-29 17:06:38 +0300

    CountDownTimer example

--- app/src/main/java/com/yusuf/datasavingwithsharedpreferences/Activity2.java
+++ app/src/main/java/com/yusuf/datasavingwithsharedpreferences/Activity2.java
@@ -4,12 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Activity2 extends AppCompatActivity {
 
     TextView textView;
+    TextView timer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,10 +22,30 @@ public class Activity2 extends AppCompatActivity {
         Intent intent = getIntent();
         String name = intent.getStringExtra("sendedName");
         textView.setText("Your Name:"+name);
+
+        timer = findViewById(R.id.timer);
     }
 
     public void goTo1Screen(View view){
         Intent intent = new Intent(this,MainActivity.class);
         startActivity(intent);
+    }
+
+    public void countTimer(View view){
+        new CountDownTimer(10000,1000){
+
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+                timer.setText("Left: "+millisUntilFinished/1000+"s");
+
+            }
+
+            @Override
+            public void onFinish() {
+                timer.setText("Time is over!");
+                Toast.makeText(Activity2.this,"Time is Over!",Toast.LENGTH_LONG).show();
+            }
+        }.start();
     }
 }
